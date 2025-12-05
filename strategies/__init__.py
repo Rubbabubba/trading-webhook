572c47d62002085db99bc230cd1e5c56e051900d
Rubbabubba <47430510+Rubbabubba@users.noreply.github.com>
2025-12-05 11:52:53 -0600

    Delete strategies directory

--- strategies/__init__.py
+++
@@ -1,6 +0,0 @@
-# strategies/__init__.py
-# Make strategies a package and expose common types.
-
-from .base import BaseStrategy, OrderPlan  # re-export for convenience
-
-__all__ = ["BaseStrategy", "OrderPlan"]
